9ca188fed082a1e59e80ba1fddce9236eb95fc8a
jeevan24111 <jeevanbahaddur616@gmail.com>
2026-07-14 07:43:10 +0530

    done datatypes

---
+++ day1/firstJ.java
@@ -0,0 +1,24 @@
+public class firstJ{
+    public static void main(String[] args) {
+        System.out.println("hello JAVA");
+
+        System.out.println("hello world");
+
+        short s=10;
+        byte b=2;
+        int i=234;
+        float f=12.3f;
+        double d=22.2323;
+        long l=123753421l;
+
+        System.out.println(s);
+        System.out.println(b);
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(l);
+
+        System.out.printf("hello naman ,"+"your age is %d",s);
+
+    }
+}
