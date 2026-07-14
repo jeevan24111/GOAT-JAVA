dda4c5b420122d9f633561807cde7bb061f5d658
jeevan24111 <jeevanbahaddur616@gmail.com>
2026-07-14 08:24:48 +0530

    done some implicit adn explicit conversion on numeric

--- day1/firstJ.java
+++ day1/firstJ.java
@@ -6,10 +6,13 @@ public class firstJ{
 
         short s=10;
         byte b=2;
-        int i=234;
-        float f=12.3f;
-        double d=22.2323;
+        int i=b;
+        double d=22.23234567565646;
+         float f=(float)d;
         long l=123753421l;
+
+        int f1=(int)d;
+        int f2=(int)f;
 
         System.out.println(s);
         System.out.println(b);
@@ -17,8 +20,25 @@ public class firstJ{
         System.out.println(f);
         System.out.println(d);
         System.out.println(l);
+        System.out.println(f1);
+        System.out.println(f2);
 
         System.out.printf("hello naman ,"+"your age is %d",s);
+        System.out.println("");
+        if(args.length>=1){
+             System.out.println("First :"+args[0]);
+            System.out.println("second :"+args[1]);
+        }
+        //Implicit casting
+        byte byteVar = 42;
+        short shortVar = byteVar;
+        int intVar = shortVar;
+
+        System.out.println(byteVar);
+        System.out.println(shortVar);
+        System.out.println(intVar);
+
+        //int badInt   = (int)  true; // Compiler error: incompatible types
 
     }
 }
