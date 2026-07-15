c8840ec24a536c86fea0e72bac304d4b65df1a40
jeevan24111 <jeevanbahaddur616@gmail.com>
2026-07-15 06:57:04 +0530

    started day 2 practcing of java now compledted operations in java

---
+++ DAY2/op.java
@@ -0,0 +1,35 @@
+public class op{
+    public static void main(String[] args) {
+      
+    
+
+  int var1 = 10;
+      byte var2 = 20;
+      byte var3 = 12;
+      System.out.println(var1 + var2);
+      System.out.println(var1 - var2);
+      System.out.println(var1 * var2);
+      System.out.println(var1 / var2);
+      System.out.println(var1 % var2);
+      System.out.println(var1 < var2 & var1 > var3);
+      System.out.println(var1 < var2 | var1 > var3);
+      System.out.println(var1 > var2);
+      System.out.println(var1 < var2);
+      System.out.println(var1 >= var2);
+      System.out.println(var1 <= var2);
+      System.out.println(var1 != var2);
+      System.out.println(var1 == var2);
+      System.out.println(var1 >> var2);
+      System.out.println(var1 << var2);
+      int var4;
+      System.out.println(var4 = var1 >> var2);
+      System.out.println(var1 = var4 << var2);
+      int var6;
+      System.out.println(var6 = var1 & var2);
+      System.out.println(var1 = var6 | var2);
+      System.out.println(var1 ^ var2);
+      System.out.println(var1 >>> var2); {
+    
+}
+}
+}
