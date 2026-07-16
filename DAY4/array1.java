2a44e448ceb4aee9c7b8e6439ecbd6c08f833463
jeevan24111 <jeevanbahaddur616@gmail.com>
2026-07-16 06:43:01 +0530

    Day4 -started arrays

---
+++ DAY4/array1.java
@@ -0,0 +1,27 @@
+public class array1 {
+    public static void main(String[] args) {
+        System.out.println("ARRAYS");
+
+        /*initially array are not there so we have to declare multiple variables to store multiple data
+        suppose we have 100 student so i have to declare 100 variables and time consuming process
+        later on array came which is a collection of multiple values of the same data types  */
+
+        //it is declared as
+        int arr[]=new int[4];
+        
+        //date_type[] array_name=new data_type[array_size];
+
+        //Assigning values to array;
+        arr[0]=1;
+        arr[1]=6;
+        arr[2]=3;
+        arr[3]=2;
+        System.out.println( arr[0]);
+        System.out.println( arr[1]);
+        System.out.println( arr[2]);
+        System.out.println( arr[3]);
+
+
+    }
+    
+}
