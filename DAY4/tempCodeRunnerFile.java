389d5cf8b222fffb1d4e044a4c1efc2fae303699
jeevan24111 <jeevanbahaddur616@gmail.com>
2026-07-25 06:12:38 +0530

    in DAY4 learnt creating 2d  arrays ,how to create,access,print and all

---
+++ DAY4/tempCodeRunnerFile.java
@@ -0,0 +1,7 @@
+ // int x=10;
+        //  for(int i=0;i<5;i++){
+        //     for(int j=0;j<5;j++){
+        //         arrTwo[i][j]=x;
+        //         x++;
+        //     }
+        // }
--- DAY4/twoarray.java
+++ DAY4/twoarray.java
@@ -2,5 +2,56 @@ package DAY4;
 public class twoarray{
     public static void main(String[] args) {
         //2d-arrays
+
+
+        //1.how to declare 2d arrays
+        int[][] aarTwo=new int[3][3];
+
+        int arr2[][]=new int[2][2];
+        arr2[0][1]=50;
+        System.out.println(arr2[0][1]);
+
+        
+
+        // //int[2][3]=2->rows,3->columns
+
+        // //2.assigning values
+        // //first row and and 3 col
+        // arrTwod[0][0]=23;
+        // arrTwod[0][1]=25;
+        // arTwo2d[0][2]=26;
+
+
+        // //2nd row and all 3 col
+        // arrTwod[1][0]=45;
+        // arrTwod[1][1]=44;
+        // arrTwod[1][2]=46;
+
+        // //printing all values of 2d array
+        // for(int i=0;i<5;i++){
+        //     for(int j=0;j<5;j++){
+        //         System.out.println(arrTwod[i][j]);
+        //     }
+        // }
+
+        //rows and col length
+        //rows length=arr.length
+        //col length=arr[0].length
+
+        //assigning value through loop
+        // int x=10;
+        //  for(int i=0;i<5;i++){
+        //     for(int j=0;j<5;j++){
+        //         arrTwo[i][j]=x;
+        //         x++;
+        //     }
+        // }
+
+
+
+        int arr3[][]=new int[2][2];
+        arr3[0][1]=50;
+        System.out.println(arr3[0][1]);
+
     }
 }
