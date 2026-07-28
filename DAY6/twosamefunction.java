9efceb7501b4b5f6be70d47fa08d354284cd6db8
jeevan24111 <jeevanbahaddur616@gmail.com>
2026-07-28 19:07:19 +0530

    learnt how to handle 2 function name with diff return type

---
+++ DAY6/twosamefunction.java
@@ -0,0 +1,24 @@
+public class twosamefunction {
+    public static void main(String[] args) {
+        /* two function having same name but diff in return type */
+        hello();
+
+        /*
+            to solve this problem ,we have two choice
+              1.change function name
+              2.make ont type casted function
+        */
+
+    }
+
+    static void hello(){
+        System.out.println("hello Bro");
+    }
+    /*  static int hello(){
+        System.out.println("hello Bro");
+
+    duplicate method hello() in type twosamefunction
+    }*/
+
+    
+}
