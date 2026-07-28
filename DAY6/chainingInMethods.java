596e7bd5c407d74e46f48fd1205048fd37b99245
jeevan24111 <jeevanbahaddur616@gmail.com>
2026-07-28 19:17:21 +0530

    learnt function chaining in java which lets other function excutes and at last called function other parts are excuted

---
+++ DAY6/chainingInMethods.java
@@ -0,0 +1,26 @@
+public class chainingInMethods {
+    public static void main(String[] args) {
+        /*chaining of functions in JAVA */
+
+
+        A();
+
+        /*calling fxn a called b fxn in it
+         and 
+         hence b fxn call c fxn and  executes all  c function
+         from program comes back to b and then a */
+    }
+    static void A(){
+        B();
+         System.out.println("I am Function A");
+
+    }
+    static void B(){
+        C();
+          System.out.println("I am Function B");  
+    }
+    static void C(){
+         System.out.println("I am Function c");
+    }
+    
+}
