f37e0aaa614a883c24b90177440a81f5e85a53b0
jeevan24111 <jeevanbahaddur616@gmail.com>
2026-07-28 19:24:34 +0530

    learnt Typecasted return type in java ,day 6 is done

---
+++ DAY6/typecastedReturnType.java
@@ -0,0 +1,13 @@
+public class typecastedReturnType {
+    public static void main(String[] args) {
+        int x=20,y=20;
+
+        double res=sum(x,y);
+        System.out.println(res);
+
+        //here i passed parameters as integer but got returned as double because of typecasting while returning
+    }
+    static double sum(int x,int y){
+        return (double)x+y;
+    }
+}
