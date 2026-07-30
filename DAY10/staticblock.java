ef2a8be58fad7d0198d15954459bb31f03bd6cd8
jeevan24111 <jeevanbahaddur616@gmail.com>
2026-07-30 09:35:30 +0530

    learnt what are static blokcs in java

---
+++ DAY10/staticblock.java
@@ -0,0 +1,35 @@
+public class staticblock {
+    public static void main(String[] args) {
+        /*   static blocks in java are used to initialize static variables in java 
+            they are executed when the class is loaded
+         static block are loaded first
+        */
+
+         System.out.println(Student.College);
+         System.out.println(Student.location);
+    }
+
+
+}
+
+class Student{
+    String name;
+    int age;
+    String gender;
+    static String College;
+    static String location;
+    Student(String name,int age,String gender){
+        this.name=name;
+        this.age=age;
+        this.gender=gender;
+    }
+
+    static{
+        College="MIT";
+        location="USA";
+    }
+
+    static void holiday(){
+        System.out.println("today is holiday\n");
+    }
+}
