a8c4fd88ce26c8cb7a0a0472e137be33d3d20194
jeevan24111 <jeevanbahaddur616@gmail.com>
2026-07-31 15:14:42 +0530

    modifief some lines in default and protected repo

--- DAY11/Encapsulation/default and protected/protectedmModifers.java
+++ DAY11/Encapsulation/default and protected/protectedmModifers.java
@@ -1,4 +1,4 @@
-package DAY11.Encapsulation.default and protected;
+// package DAY11.Encapsulation.default and protected;
 
 public class protectedmModifers {
     public static void main(String[] args) {
