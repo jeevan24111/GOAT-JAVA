12916dd456a8e1731bae3177978b292a2b32ab19
jeevan24111 <jeevanbahaddur616@gmail.com>
2026-07-31 16:15:40 +0530

    learnt hierarchical inheritance in java

---
+++ DAY11/Inheritance/hierarchicalInheritance.java
@@ -0,0 +1,45 @@
+// package DAY11.Inheritance;
+
+public class hierarchicalInheritance {
+    public static void main(String[] args) {
+
+
+        /* hierarchical inheritance 
+                   A
+                 /   \
+                B     C
+        */
+
+
+                dad d1=new dad();
+                child21 c1=new child21();
+                child22 c2=new child22();
+
+                System.out.println(d1.name);
+                System.out.println(c1.name);
+                     c1.parent1();
+                System.out.println(c2.name);
+                c2.parent1();
+        
+    }
+    
+}
+
+class dad{
+    String name="DAD";
+
+}
+
+class child21 extends dad{
+
+    void parent1(){
+        System.out.println("My parent name is : "+name);
+    }
+}
+
+class child22 extends dad{
+     void parent1(){
+        System.out.println("My parent name is : "+name);
+    }
+
+}
