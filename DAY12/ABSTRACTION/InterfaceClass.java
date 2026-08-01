f45acdf0a493c68d74fe82a644a112790f503510
jeevan24111 <jeevanbahaddur616@gmail.com>
2026-08-01 08:32:11 +0530

    java interface how they works

---
+++ DAY12/ABSTRACTION/InterfaceClass.java
@@ -0,0 +1,31 @@
+public class InterfaceClass {
+    public static void main(String[] args) {
+        /* interface means pure abstraction give undefined method 
+         and if anyone to inherit to it,they have all the methods in them 
+        */
+        bike b1=new splendor();
+        b1.gear();
+        b1.accelerator();
+        b1.breakk();
+    }
+}
+
+interface bike{
+    abstract  void gear();
+    abstract  void accelerator();
+    abstract   void breakk();
+}
+
+class splendor implements bike{
+
+    //this class needs to implement all methods
+   public  void gear(){  //  if you don't give public it will give visibilty error to bike
+        System.out.println(" i have 4 gear and approach back to increase gear");
+    }
+   public   void accelerator(){
+         System.out.println(" i am accelerating at 120 speed");
+    }
+    public void breakk(){
+             System.out.println(" i am at high speed wait");
+    }
+}
