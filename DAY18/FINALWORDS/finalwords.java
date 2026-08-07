5871983224fb62995eb64110637eeaeac0fe6e95
jeevan24111 <jeevanbahaddur616@gmail.com>
2026-08-07 21:30:31 +0530

    finished java Object class

---
+++ DAY18/FINALWORDS/finalwords.java
@@ -0,0 +1,22 @@
+package DAY18.FINALWORDS;
+
+public class finalwords {
+    public static void main(String[] args) {
+        /* we will  learn more on object class 
+         
+
+        imp->
+
+        1.Object and Objects are diff
+           ->Object is a parent class
+           ->objects is utility class
+        2.Array and Arrays are diff 
+
+        int,char,boolean why these don't have parent class object
+        coz it is belong primitive and object class belong to class not to  primitive 
+
+        but Integer,Float,Character have object class they  are non primitive and and they are in heap
+        */
+    }
+    
+}
