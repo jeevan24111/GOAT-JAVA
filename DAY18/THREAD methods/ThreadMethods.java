63cbfd5db393212a840841d13bbf0859855fecbb
jeevan24111 <jeevanbahaddur616@gmail.com>
2026-08-07 21:24:54 +0530

    JAVA THREADS METHODS OF OBJECT CLASS

---
+++ DAY18/THREAD methods/ThreadMethods.java
@@ -0,0 +1,15 @@
+package DAY18.THREAD methods;
+
+public class ThreadMethods {
+    public static void main(String[] args) {
+        /*
+           threads have 3 objects methods
+           1.wait();
+           2.notify();
+           3.notifyALl();
+
+           we will study more on about these methods in threads
+        */
+    }
+    
+}
